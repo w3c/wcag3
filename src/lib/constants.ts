3ec87f437bd2b7c32cbdbaf00b3cdd3bb951d2b4
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-08-13 13:30:12 -0400

    Implement client-side filtering for previews

---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+/** Indicates that the build is running in a dev or PR preview environment. */
+export const isDevOrPreview = import.meta.env.DEV || import.meta.env.NETLIFY;
--- src/lib/guidelines.ts
+++ src/lib/guidelines.ts
@@ -1,15 +1,23 @@
 import { getCollection, getEntry, type CollectionEntry, type CollectionKey } from "astro:content";
 import capitalize from "lodash-es/capitalize";
+import difference from "lodash-es/difference";
+
+import { isDevOrPreview } from "./constants";
 
 /**
- * Returns a filtered list of requirement/assertion slugs under the given group and guideline,
- * excluding any marked as needing additional research if WCAG_SKIP_WIP is set.
+ * Returns a list of requirement/assertion slugs under the given group and guideline that would be
+ * skipped for a major publication (i.e. exploratory/placeholder or needs additional research).
+ * Note this returns an empty array when building for situations that do not allow for skipping
+ * either at build time or client-side (e.g. Editor's Drafts).
  */
-async function getFilteredRequirements(groupId: string, guidelineSlug: string) {
+async function determineSkippableRequirements(groupId: string, guidelineSlug: string) {
+  // Only populate when running a build that needs to skip at build time or client-side
+  if (!import.meta.env.WCAG_SKIP_WIP && !isDevOrPreview) return [];
+
   const guideline = await getEntry("guidelines", `${groupId}/${guidelineSlug}`);
   if (!guideline) throw new Error(`Unresolvable guideline ID: ${guidelineSlug}`);
 
-  const filteredRequirements: string[] = [];
+  const skippableRequirements: string[] = [];
   for (const requirementSlug of guideline.data.children) {
     const requirement = await getEntry(
       "requirements",
@@ -17,21 +25,25 @@ async function getFilteredRequirements(groupId: string, guidelineSlug: string) {
     );
     if (!requirement) throw new Error(`Unresolvable requirement ID: ${requirementSlug}`);
     if (
-      import.meta.env.WCAG_SKIP_WIP &&
-      (requirement.data.needsAdditionalResearch ||
-        requirement.data.status === "placeholder" ||
-        requirement.data.status === "exploratory")
-    )
-      continue;
-    filteredRequirements.push(requirementSlug);
+      requirement.data.needsAdditionalResearch ||
+      requirement.data.status === "placeholder" ||
+      requirement.data.status === "exploratory"
+    ) {
+      skippableRequirements.push(requirementSlug);
+    }
   }
-  return filteredRequirements;
+  return skippableRequirements;
+}
+
+/** Extension of Requirement to facilitate client-side skipping in previews */
+interface SkippableRequirement extends CollectionEntry<"requirements"> {
+  skippable?: boolean;
 }
 
 let groupIds = (await getCollection("groupOrder")).map(({ id }) => id);
 let groups: Record<string, CollectionEntry<"groups">> = {};
 let guidelines: Record<string, CollectionEntry<"guidelines">> = {};
-let requirements: Record<string, CollectionEntry<"requirements">> = {};
+let requirements: Record<string, SkippableRequirement> = {};
 
 export async function buildGuidelinesHierarchy() {
   // Cache collated collection data for subsequent calls
@@ -44,8 +56,12 @@ export async function buildGuidelinesHierarchy() {
       for (const guidelineSlug of group.data.children) {
         const guideline = await getEntry("guidelines", `${groupId}/${guidelineSlug}`);
         if (!guideline) throw new Error(`Unresolvable guideline ID: ${guidelineSlug}`);
-        guideline.data.children = await getFilteredRequirements(groupId, guidelineSlug);
         guidelines[guideline.id] = guideline;
+
+        const skippableChildren = await determineSkippableRequirements(groupId, guidelineSlug);
+        if (import.meta.env.WCAG_SKIP_WIP)
+          // Filter children directly in case of build-time skip
+          guideline.data.children = difference(guideline.data.children, skippableChildren);
 
         for (const requirementSlug of guideline.data.children) {
           const requirement = await getEntry(
@@ -53,7 +69,9 @@ export async function buildGuidelinesHierarchy() {
             `${groupId}/${guidelineSlug}/${requirementSlug}`
           );
           if (!requirement) throw new Error(`Unresolvable requirement ID: ${requirementSlug}`);
-          requirements[requirement.id] = requirement;
+          requirements[requirement.id] = skippableChildren.includes(requirementSlug)
+            ? { ...requirement, skippable: true }
+            : requirement;
         }
       }
     }
