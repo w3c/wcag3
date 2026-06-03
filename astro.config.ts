e61281fc18a5d150203e79d949cd896d3082bba5
Kevin White <kevin@w3.org>
2026-06-03 14:43:10 +0100

    Update astro.config.ts
    Will commit this... I confess to not liking the github-light but we can return to this
    
    Co-authored-by: Adam Page <adamfpage@gmail.com>

--- astro.config.ts
+++ astro.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       handlers: { ...defListHastHandlers },
     },
     shikiConfig: {
-      theme: 'night-owl-light',
+      theme: 'github-light-high-contrast',
     },
   },
   experimental: {
